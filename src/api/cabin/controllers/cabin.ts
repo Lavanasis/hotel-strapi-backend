7127d0ab722303c8c27287e9e73146f66870a8fc
Lavana <952982301@qq.com>
2025-07-18 00:37:11 +0800

    Init: Migrate backend project

---
+++ src/api/cabin/controllers/cabin.ts
@@ -0,0 +1,7 @@
+/**
+ * cabin controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cabin.cabin');
---
+++ src/api/cabin/routes/cabin.ts
@@ -0,0 +1,7 @@
+/**
+ * cabin router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cabin.cabin');
